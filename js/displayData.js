fcad6b0708bf2998fa89aab84de83feaafd4bfd9
Donnie Plumly <donnie.plumly@gmail.com>
2024-05-14 10:37:33 -0700

    css
    renamed html files, css updates and cleaned up js

--- js/displayData.js
+++ js/displayData.js
@@ -1,10 +1,25 @@
 console.log('Start display data script');
 
+
 ////////////////////////////////////////////////////////////////////////////
 // Displays the number
 ////////////////////////////////////////////////////////////////////////////
 
-setInterval(() => {
-    let savedNumber = localStorage.getItem("savedNumber");
-    document.getElementById("displayArea").innerText = "Snowflake Save the Bears: " + savedNumber;
-  }, 1000); 
+// Update the display every second
+setInterval(updateDisplay, 1000);
+
+// Function to update the display
+function updateDisplay() {
+  let savedNumber = localStorage.getItem("savedNumber") || 0;
+  document.getElementById("displayArea").innerText = "Snowflake Save the Bears: " + savedNumber;
+  console.log('Updated value: ' + savedNumber);
+}
+
+// Set savedNumber to 0 if it's null
+function setToZero(savedNumber) {
+  if (savedNumber === null) {
+    console.log('Set to 0');
+    savedNumber = 0;
+  }
+  return savedNumber;
+}
--- js/saveData.js
+++ js/saveData.js
@@ -1,31 +1,25 @@
 console.log('Start save data script');
 
-let number = localStorage.getItem('savedNumber');
 
+// Load current number when the window loads
 window.addEventListener("load", () => {
+    let number = localStorage.getItem('savedNumber') || 0;
     document.getElementById("currentNumber").innerText = "Current number is: " + number;
-  });
+});
 
-////////////////////////////////////////////////////////////////////////////
-// Saves the number
-////////////////////////////////////////////////////////////////////////////
-
+// Save the number when the save button is clicked
 document.getElementById("saveButton").addEventListener('click', () => {
     let number = document.getElementById("numberInput").value;
     localStorage.setItem("savedNumber", number);
 
     document.getElementById("currentNumber").innerText = "Current number is: " + number;
-    document.getElementById('numberInput').value = null;
+    console.log('Updated value: ' + number);
 });
 
-////////////////////////////////////////////////////////////////////////////
-// Reset the number to 0
-////////////////////////////////////////////////////////////////////////////
+// Reset the number to 0 when the reset button is clicked
 document.getElementById('resetButton').addEventListener('click', () => {
     localStorage.clear();
     document.getElementById('numberInput').value = 0;
-    document.getElementById("currentNumber").innerText = "Current number is: " + 0;
+    document.getElementById("currentNumber").innerText = "Current number is: 0";
+    console.log('Number reset to 0');
 });
-
-
-
